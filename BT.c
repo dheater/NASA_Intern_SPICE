51a4da7685554490a98f2bd69a8c02d8d22717a3
Riley Waite <rileybrains@gmail.com>
2015-06-04 12:10:15 -0400

    Basic Testing Files

---
+++ BT.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "/SPICE/cspice/src/cspice/furnsh_c.c"
+
+#define SIZESTR 100
+
+int main(){
+
+	SpiceChar str[SIZESTR];
+
+	printf("\n\n\tRunning...\n\n");
+	prompt_c("Enter setup file name: ", SIZESTR, str);
+	
+	if(str[0]=='q'){
+		printf("\n\n\tKF.txt is loading to kernel pool...\n");
+		furnsh_c("KF.txt");
+	}else{
+		printf("\n\n\t%s is loading to kernel pool...\n", str);
+		furnsh_c(str);
+	}
+
+	printf("\n\n\tCompleted...\n");
+}
